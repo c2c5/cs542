ea3d4c8f30f0f6e3793e8f8687e1c83af25bc62d
Ryan Johnson <ryan@johnsonweb.us>
2019-10-15 00:22:04 -0400

    Pagination, User editing

---
+++ migrations/20191014_01_GoMDS-trigger-password-change-logout.py
@@ -0,0 +1,17 @@
+"""
+trigger-password-change-logout
+"""
+
+from yoyo import step
+
+__depends__ = {'20191013_01_z7Cn9-cascade-userroles-delete'}
+
+steps = [
+step("""CREATE TRIGGER logout_upon_password_change AFTER UPDATE ON cs542.User
+FOR EACH ROW
+BEGIN
+	IF NOT (NEW.password_hash <=> OLD.password_hash) THEN
+		DELETE FROM LoginSession L WHERE L.userid=NEW.userid;
+    END IF;
+END;""")
+]
