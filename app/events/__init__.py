3dee0dad4a62d490eb247541f3069082043e5ca0
888king <m13001278911@163.com>
2019-10-21 15:08:38 -0400

    add edit

--- app/events/__init__.py
+++ app/events/__init__.py
@@ -27,8 +27,40 @@ def show_info(id):
     with db.cursor() as cursor:
         get_event_info = "SELECT name, start, end, description, max_participants, cost, paid_members_only FROM event WHERE eventid=%s;"
         cursor.execute(get_event_info, id)
-        info = cursor.fetchall()
-        return render_template('event_info.html', entries=info)
+        entries = cursor.fetchall()
+        return render_template('event_info.html', entries=entries)
+
+@events.route('/edit/<id>', methods=["GET", "POST"])
+def edit(id):
+    if request.method == "GET":
+        db = database.get_db()
+        with db.cursor() as cursor:
+            get_event_info = "SELECT name, start, end, description, max_participants, cost, paid_members_only FROM event WHERE eventid=%s;"
+            cursor.execute(get_event_info, id)
+            entries = cursor.fetchall()
+            get_opener = "SELECT userid, student_name from userdatawithrole where roles = 'opener' or roles = 'admin'"
+            cursor.execute(get_opener)
+            openers = cursor.fetchall()
+        try:
+            return render_template('event_edit.html', entries=entries, openers=openers)
+        except TemplateNotFound:
+            abort(500)
+    else:
+        db = database.get_db()
+        with db.cursor() as cursor:
+            update_event = "UPDATE event SET name=%s, description=%s, start=%s, end=%s, max_participants=%s, cost=%s, paid_members_only=%s " + \
+                "WHERE eventid=%s;"
+            print(request.form['event_name'], request.form['description'],
+                                             request.form['start'], request.form['end'], request.form['max_participants'],
+                                             request.form['cost'], request.form['PMO_Options'], id)
+            cursor.execute(update_event, (request.form['event_name'], request.form['description'],
+                                             request.form['start'], request.form['end'], request.form['max_participants'],
+                                             request.form['cost'], request.form['PMO_Options'], id))
+            print(cursor.rowcount)
+            if (cursor.rowcount == 1):
+                db.commit()
+                flash('Changed event infomation', 'success')
+                return redirect(url_for('events.show_info', id=id, **request.args))
 
 @events.route('/create', methods=["GET", "POST"])
 def create():
@@ -47,7 +79,7 @@ def create():
                                              request.form['cost'], request.form['PMO_Options']))
             if (cursor.rowcount == 1):
                 db.commit()
-                flash('Created user account', 'success')
+                flash('Created event', 'success')
                 return redirect(url_for('events.show', **request.args))
             else:
                 flash('Error creating user.', 'danger')
